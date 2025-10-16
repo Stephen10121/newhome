f3930925aa993d4f153ae8144b3acbd3cff77509
Stephen Gruzin <stephengrzn@gmail.com>
2025-10-16 03:36:06 -0700

    Added dockerfile

--- svelte.config.js
+++ svelte.config.js
@@ -14,6 +14,9 @@ const config = {
 		adapter: adapter(),
 		alias: {
 			"@/*": "./src/lib/*",
+		},
+		csrf: {
+			trustedOrigins: ["https://stephengruzin.dev", "https://www.stephengruzin.dev"]
 		}
 	}
 };
